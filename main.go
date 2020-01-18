dcda1b0fa3ad6905ad8e170cb0c5b9526aa92f20
Robert <rkhousadian@gmail.com>
2020-01-18 10:27:59 -0800

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,47 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var Token string
+
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+
+}
+
 func main() {
-	
+	var Token string = "" //discord bot token goes here
+	dg, err := discordgo.New("Bot " + Token)
+	if err != nil {
+		fmt.Println("Error occurred creating discord session - ", err)
+		return
+	}
+
+	dg.AddHandler(messageCreate)
+
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("Error occurred opening the connection - ", err)
+		return
+	}
+
+	fmt.Println("Gary Bot is now live!\nPress CTRL+C to quit the program.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	dg.Close()
+}
+
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+
 }
